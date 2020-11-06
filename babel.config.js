fe36103e2e3b7ceff4ff02de6a70a8f2b32bd309
Nana Adane <nanaofosuheneadane@gmail.com>
2020-11-06 22:11:04 +0000

    chore(*): setup build sytem - babel

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+const presets = [
+    [
+        '@babel/preset-env',
+        {
+            targets: {
+                node: 'current',
+            },
+        },
+    ],
+];
+
+const plugins = ['add-module-exports'];
+
+module.exports = {
+    presets,
+    plugins,
+    sourceMaps: 'inline',
+};
